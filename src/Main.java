2c658e8dc2f9773f4bcebabef6dcc1a423db3150
Lilnurs <zulpukarov518@icloud.com>
2022-10-05 11:19:27 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int ageOfMan = 30;
+        int temperature = 25;
+        if (ageOfMan > 25 && temperature > 20) {
+            System.out.println("Можно идти гулять");
+            if (ageOfMan > 18 && temperature > 24) {
+                System.out.println("Можно идти гулять");
+                if (ageOfMan < 50 && temperature > 23);
+                System.out.println("Можно идти гулять");
+
+                if (ageOfMan < 40 && temperature < 28);
+                System.out.println("Остовайтесь дома");
+
+                if (ageOfMan < 40 && temperature < 28);
+                System.out.println("Остовайтесь дома");
+
+                if (ageOfMan < 35 && temperature < 30);
+                System.out.println("Остовайтесь дома");
+
+                if (ageOfMan < 50 && temperature < 32);
+                System.out.println("Остовайтесь дома");
+
+                if (ageOfMan < 42 && temperature < 38);
+                System.out.println("Остовайтесь дома");
+            }
+
+            }
+        }
+    }
